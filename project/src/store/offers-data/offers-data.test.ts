9daf75b62dc8192706464943b37cd789503c078f
Ciberian <isi04047891@gmail.com>
2022-08-26 15:46:18 +0500

    Добавляет тесты offers-data редьюсера, на получение одного оффера

--- project/src/store/offers-data/offers-data.test.ts
+++ project/src/store/offers-data/offers-data.test.ts
@@ -1,9 +1,10 @@
 import { offersData } from './offers-data';
 import { OfferData } from '../../types/state';
-import { fetchOffersAction } from '../api-actions';
+import { fetchOffersAction, fetchOfferAction } from '../api-actions';
 import { makeFakeOffer } from '../../utils/mocks';
 
 const fakeOffers = new Array(10).fill(null).map(() => (makeFakeOffer()));
+const fakeOffer = makeFakeOffer();
 
 describe('Reducer: offersData', () => {
   let state: OfferData;
@@ -40,21 +41,18 @@ describe('Reducer: offersData', () => {
     });
   });
 
-  // describe('loginAction test', () => {
-  //   it('should update authorizationStatus to "AUTH" and userInfo if loginAction fulfilled', () => {
-  //     expect(userProcess.reducer(state, { type: loginAction.fulfilled.type, payload: userInfo }))
-  //       .toEqual({authorizationStatus: AuthorizationStatus.Auth, userInfo: userInfo});
-  //   });
-  //   it('should update authorizationStatus to "NO_AUTH" if loginAction rejected', () => {
-  //     expect(userProcess.reducer(state, { type: loginAction.rejected.type}))
-  //       .toEqual({authorizationStatus: AuthorizationStatus.NoAuth, userInfo: null});
-  //   });
-  // });
-
-  // describe('logoutAction test', () => {
-  //   it('should update authorizationStatus to "NO_AUTH" if logoutAction fulfilled', () => {
-  //     expect(userProcess.reducer(state, { type: logoutAction.fulfilled.type }))
-  //       .toEqual({authorizationStatus: AuthorizationStatus.NoAuth, userInfo: null});
-  //   });
-  // });
+  describe('fetchOfferAction test', () => {
+    it('should not update state if fetchOfferAction pending', () => {
+      expect(offersData.reducer(state, {type: fetchOfferAction.pending.type}))
+        .toEqual(state);
+    });
+    it('should update offer to "fakeOffer" and loading status to "true" if fetchOfferAction fullfilled', () => {
+      expect(offersData.reducer(state, {type: fetchOfferAction.fulfilled.type, payload: fakeOffer}))
+        .toEqual({...state, offer: fakeOffer, isOfferLoaded: true});
+    });
+    it('should update loading status to "true" if fetchOfferAction rejected', () => {
+      expect(offersData.reducer(state, {type: fetchOfferAction.rejected.type}))
+        .toEqual({...state, isOfferLoaded: true});
+    });
+  });
 });
