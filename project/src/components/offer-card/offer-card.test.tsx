bab22488304cca7622a56c39b48df7dacbd98472
Ciberian <isi04047891@gmail.com>
2022-08-29 11:00:05 +0500

    Создаёт компонет history-route и использует его в тесте

---
+++ project/src/components/offer-card/offer-card.test.tsx
@@ -0,0 +1,24 @@
+import '@testing-library/jest-dom/extend-expect';
+import HistoryRouter from '../../history-route/history-route';
+import { render, screen } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
+import { makeFakeOffer } from '../../utils/mocks';
+import { Provider } from 'react-redux';
+import { store } from '../../store';
+import OfferCard from './offer-card';
+
+const fakeOffer = makeFakeOffer(0);
+const history = createMemoryHistory();
+
+describe('Component: OfferCard', () => {
+  it('should render correctly', () => {
+    render(
+      <Provider store={store}>
+        <HistoryRouter history={history}>
+          <OfferCard offer={fakeOffer} classPrefix='cities' />
+        </HistoryRouter>
+      </Provider>);
+
+    expect(screen.getByText(fakeOffer.title)).toBeInTheDocument();
+  });
+});
